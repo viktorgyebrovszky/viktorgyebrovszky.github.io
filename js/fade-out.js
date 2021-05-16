a0be6be39c5e8f41d480915f69d0b3987e25dee8
viktorgyebrovszky <viktorgoa@gmail.com>
2021-05-16 02:57:40 +0100

    ch

--- js/fade-out.js
+++ js/fade-out.js
@@ -6,7 +6,7 @@ window.onscroll = function() {
   var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 
   // Change this if you want it to fade faster
-  height = height / 2;
+  height = height / 3;
 
   target.style.opacity = (height - scrollTop) / height;
 
